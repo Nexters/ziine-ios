7f486a6a710d3620509e162b2ef699b176ec5c0b
GeonWoo <lgw9898@naver.com>
2025-01-28 01:54:14 +0900

    #2 added artwork rib

---
+++ Features/ArtworkFeature/ArtworkFeature/Source/ArtworkViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkViewController.swift
+//  ArtworkFeature
+//
+//  Created by Geon Woo lee on 1/28/25.
+//
+
+import UIKit
+
+final class ArtworRouter { }
+
+final class ArtworkViewBuilder { }
+
+final class ArtworkInteractor { }
+
+final class ArtworkViewController: UIViewController {
+    
+}
