73db863325e923c663a83234ef36d0c2483cb593
GeonWoo <lgw9898@naver.com>
2025-01-27 23:23:12 +0900

    #2 add feature interface

---
+++ Features/ArtworkFeature/ArtworkFeature/ArtworkFeature.h
@@ -0,0 +1,18 @@
+//
+//  ArtworkFeature.h
+//  ArtworkFeature
+//
+//  Created by Geon Woo lee on 1/27/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArtworkFeature.
+FOUNDATION_EXPORT double ArtworkFeatureVersionNumber;
+
+//! Project version string for ArtworkFeature.
+FOUNDATION_EXPORT const unsigned char ArtworkFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArtworkFeature/PublicHeader.h>
+
+
---
+++ Features/PostingFeature/PostingFeature/PostingFeature2.swift
@@ -0,0 +1,10 @@
+//
+//  PostingFeature2.swift
+//  PostingFeature2
+//
+//  Created by Geon Woo lee on 1/27/25.
+//
+
+class PostingFeature2 {
+
+}
