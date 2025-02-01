877d135e8ab95245363e3cd0120e29f89ba744b5
GeonWoo <lgw9898@naver.com>
2025-02-01 14:57:24 +0900

    #7 UITabView 형태로 구현

--- App/ZiineApp/AppRoot/AppRootBuilder.swift
+++ App/ZiineApp/AppRoot/AppRootBuilder.swift
@@ -91,7 +91,9 @@ final class AppRootTabBarController: UITabBarController, AppRootTabBarControllab
         
         tabBar.isTranslucent = false
         tabBar.tintColor = .black
-        tabBar.backgroundColor = .white
+        tabBar.backgroundColor = .green
+        
+        print(self.tabBar.frame.height)
     }
     
     func setViewControllers(_ viewControllers: [UIViewController]) {
@@ -100,5 +102,71 @@ final class AppRootTabBarController: UITabBarController, AppRootTabBarControllab
     
     func build() -> UITabBarController {
         return self
+    }
+}
+
+
+
+struct ZiineTabBarItem {
+    var tag: Int
+    var image: UIImage
+    var selectedImage: UIImage
+}
+
+final class ZiineTabView: UIView {
+    
+    private var tabBarItems: [ZiineTabBarItem] = []
+    private var uiButtons: [UIButton] = []
+    
+    // MARK: - Initialize
+    
+    init() {
+        self.tabBarItems = [
+            .init(tag: 0, image: .actions, selectedImage: .actions),
+            .init(tag: 1, image: .add, selectedImage: .add)
+        ]
+        
+        var buttons: [UIButton] = []
+        tabBarItems.forEach { tabBarItem in
+            let button = UIButton()
+            button.setImage(tabBarItem.image, for: .normal)
+            button.setImage(tabBarItem.selectedImage, for: .selected)
+            
+            buttons.append(button)
+        }
+        
+        self.uiButtons = buttons
+        
+        super.init(frame: .zero)
+        
+        uiButtons.forEach { button in
+            button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        }
+        
+        configureUI()
+    }
+    
+    @objc func didTapButton() { }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UIComponents
+    
+    private let stackView: UIStackView = {
+        $0.distribution = .fillEqually
+        return $0
+    }(UIStackView())
+    
+    private func configureUI() {
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.heightAnchor.constraint(equalToConstant: 70)
+        ])
     }
 }
