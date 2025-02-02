90251529966f92b12c366a67681e0264ead05dbb
GeonWoo <lgw9898@naver.com>
2025-02-03 02:41:50 +0900

    #8 add usecase interface

--- Features/ArtworkFeature/ArtworkFeature/Domain/FetchArtworkListUseCase.swift
+++ Features/ArtworkFeature/ArtworkFeature/Domain/FetchArtworkListUseCase.swift
@@ -8,7 +8,12 @@
 import Utils
 import ArtworkFeatureInterface
 
-final actor FetchArtworkListUseCase {
+public protocol FetchArtworkListUseCase {
+    func fetch() async -> Result<ArtworkModel, RepositoryError>
+    func pagination() async -> Result<ArtworkModel, RepositoryError>
+}
+
+final actor DefaultFetchArtworkListUseCase: FetchArtworkListUseCase {
     
     private let artworkRepository: ArtworkRepository
     
@@ -21,6 +26,16 @@ final actor FetchArtworkListUseCase {
     private var page: Int
     
     func fetch() async -> Result<ArtworkModel, RepositoryError> {
+        page = 1
+        return await execute(page: page)
+    }
+    
+    func pagination() async -> Result<ArtworkModel, RepositoryError> {
+        page += 1
+        return await fetch()
+    }
+    
+    private func execute(page: Int) async -> Result<ArtworkModel, RepositoryError> {
         let result = await artworkRepository.fetch(query: .init(page: page))
         switch result {
         case .success(let success):
@@ -29,29 +44,4 @@ final actor FetchArtworkListUseCase {
             return .failure(message)
         }
     }
-    
-    func pagination() async -> Result<ArtworkModel, RepositoryError> {
-        page += 1
-        return await fetch()
-    }
 }
-
-//protocol DeleteProfileUseCase {
-//    func execute(requestId: String) -> AnyPublisher<String, RepositoryError>
-//}
-//
-//final class DefaultDeleteProfileUseCase: DeleteProfileUseCase {
-//    
-//    private let profileRepository: ProfileRepository
-//    
-//    init(profileRepository: ProfileRepository) {
-//        self.profileRepository = profileRepository
-//    }
-//    
-//    func execute(requestId: String) -> AnyPublisher<String, RepositoryError> {
-//        let query = DeleteProfileQuery(profileId: requestId)
-//        return profileRepository.deleteProfile(query: query)
-//            .receive(on: DispatchQueue.main)
-//            .eraseToAnyPublisher()
-//    }
-//}
