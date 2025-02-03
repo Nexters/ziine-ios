f72aa2a3b37af7cbcb2a08307483dd75ca971bea
GeonWoo Lee <2dubu.dev@gmail.com>
2025-02-03 18:43:36 +0900

    #11 Magazine 모듈 추가

---
+++ Features/MagazineFeature/MagazineFeatureInterface/MagazineFeatureInterface.h
@@ -0,0 +1,18 @@
+//
+//  MagazineFeatureInterface.h
+//  MagazineFeatureInterface
+//
+//  Created by 이건우 on 2/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagazineFeatureInterface.
+FOUNDATION_EXPORT double MagazineFeatureInterfaceVersionNumber;
+
+//! Project version string for MagazineFeatureInterface.
+FOUNDATION_EXPORT const unsigned char MagazineFeatureInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagazineFeatureInterface/PublicHeader.h>
+
+
