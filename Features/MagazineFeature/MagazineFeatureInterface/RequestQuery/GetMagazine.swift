e2fcc83877e9e10ec994a91f8ad33dcef9289a40
GeonWoo Lee <2dubu.dev@gmail.com>
2025-02-04 16:57:35 +0900

    #11 Request query

---
+++ Features/MagazineFeature/MagazineFeatureInterface/RequestQuery/GetMagazine.swift
@@ -0,0 +1,10 @@
+//
+//  GetMagazine.swift
+//  MagazineFeature
+//
+//  Created by 이건우 on 2/4/25.
+//
+
+extension MagazineRequestQuery {
+    public struct GetMagazine { }
+}
---
+++ Features/MagazineFeature/MagazineFeatureInterface/RequestQuery/MagazineRequestQuery.swift
@@ -0,0 +1,8 @@
+//
+//  MagazineRequestQuery.swift
+//  MagazineFeature
+//
+//  Created by 이건우 on 2/4/25.
+//
+
+public enum MagazineRequestQuery { }
