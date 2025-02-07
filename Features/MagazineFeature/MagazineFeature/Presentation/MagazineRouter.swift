e7987b5e046c1133e94e9031b11f7b82fbd42b1f
GeonWoo Lee <2dubu.dev@gmail.com>
2025-02-07 20:47:22 +0900

    #11 Magazine Router

---
+++ Features/MagazineFeature/MagazineFeature/Presentation/MagazineRouter.swift
@@ -0,0 +1,25 @@
+//
+//  MagazineRouter.swift
+//  MagazineFeature
+//
+//  Created by 이건우 on 2/7/25.
+//
+
+import UIKit
+import MagazineFeatureInterface
+
+final class MagazineRouter: MagazineRouting {
+    
+    var viewController: UIViewController 
+    
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+    
+    // MARK: - Detail View (web)
+    func pushToDetailView() {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .systemBlue
+        viewController.present(vc, animated: true)
+    }
+}
