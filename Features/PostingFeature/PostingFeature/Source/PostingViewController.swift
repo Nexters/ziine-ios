dd5bfc0270f83a8c1bbf6c3ef0258b1b230eb4d7
GeonWoo Lee <2dubu.dev@gmail.com>
2025-02-13 21:12:12 +0900

    #19 PostingGuideView

--- Features/PostingFeature/PostingFeature/Source/PostingViewController.swift
+++ Features/PostingFeature/PostingFeature/Source/PostingViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwiftUI
 import ThirdParty
+import DesignSystem
 internal import SnapKit
 import PostingFeatureInterface
 
@@ -33,17 +34,84 @@ final class PostingViewController: UIViewController,
     
     // MARK: - UIComponents
     
-    private func configureUI() {
-        configureTabBar()
-    }
-    
-    private func configureTabBar() {
-        tabBarItem = UITabBarItem(
-            title: nil,
-            image: UIImage(systemName: "person"),
-            selectedImage: UIImage(systemName: "person.fill")
+    private let postingGuideView: UIView = {
+        let hostingController = UIHostingController(
+            rootView: PostingGuideView()
         )
+        hostingController.view.backgroundColor = .clear
+        return hostingController.view
+    }()
+    
+    private func configureUI() {
+        view.backgroundColor = ZiineColor.uiColor(.g900)
+        
+        view.addSubview(postingGuideView)
+        postingGuideView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
 
-//struct Posting
+fileprivate struct PostingGuideView: View {
+    let guideMessage: [PostingGuideMessage] = PostingGuideMessage.make()
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center, spacing: 12) {
+                Text("작가님의 작품을 등록하기 전,")
+                Text("꼭 확인해주세요!")
+            }
+            .font(Font(ZiineFont.h4))
+            .foregroundStyle(.white)
+            .padding(.vertical, 40)
+            
+            ForEach(Array(guideMessage.enumerated()), id: \ .offset) { index, message in
+                PostingGuideCell(index: index + 1, message: message)
+            }
+        }
+        .containerRelativeFrame(.horizontal)
+        .background(ZiineColor.color(.g900))
+    }
+}
+
+fileprivate struct PostingGuideCell: View {
+    private var guideMessage: PostingGuideMessage
+    private var index: Int
+    
+    init(index: Int, message: PostingGuideMessage) {
+        self.index = index
+        self.guideMessage = message
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("0\(index)")
+                .font(Font(ZiineFont.s1))
+                .foregroundStyle(.white)
+                .padding(.bottom, 4)
+            
+            Text(guideMessage.title)
+                .font(Font(ZiineFont.s1))
+                .padding(.bottom, 8)
+                .foregroundStyle(.white)
+            
+            Text(guideMessage.description)
+                .font(Font(ZiineFont.p3))
+                .foregroundStyle(.white)
+                .padding(.bottom, 14)
+            
+            Image(systemName: "chevron.right")
+                .resizable()
+                .frame(height: 240)
+                .padding(.horizontal, 11.5)
+                .padding(.bottom, 24)
+                .foregroundStyle(.white)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 16)
+    }
+}
+
+#Preview {
+    PostingGuideView()
+}
