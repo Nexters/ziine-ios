3504419e4c02b8524f7d9b8c89d6c42edfcf5b9c
GeonWoo Lee <2dubu.dev@gmail.com>
2025-02-14 15:02:13 +0900

    #22 Remove infinity scroll

--- Features/MagazineFeature/MagazineFeature/Presentation/MagazineViewController.swift
+++ Features/MagazineFeature/MagazineFeature/Presentation/MagazineViewController.swift
@@ -16,13 +16,6 @@ protocol MagazineViewPresentableListener: AnyObject {
     func itemSelected(indexPath: IndexPath)
 }
 
-enum CarouselConstants {
-    static let dummyItemCount = 2
-    static let sideItemScale: CGFloat = 0.9
-    static let spacing: CGFloat = 8
-    static let visibleItemWidth: CGFloat = 0.8
-}
-
 final class MagazineViewController: UIViewController {
     var listener: MagazineViewPresentableListener?
     
@@ -39,19 +32,40 @@ final class MagazineViewController: UIViewController {
         }
     }
     
-    private var actualPage: Int {
-        return (currentPage - CarouselConstants.dummyItemCount).clamped(to: 1...data.count)
-    }
-    
     init() {
         super.init(nibName: nil, bundle: nil)
         
         configureUI()
-        configureInfinityScroll()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        view.backgroundColor = ZiineColor.uiColor(.g900)
+        
+        view.addSubview(magazineCarousel)
+        magazineCarousel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(24)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        view.addSubview(carouselPageCounter)
+        carouselPageCounter.snp.makeConstraints {
+            $0.top.equalTo(magazineCarousel.snp.bottom).inset(-12)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(82)
+        }
+        
+        updatePageCounter()
+    }
+    
+    private func updatePageCounter() {
+        carouselPageCounter.setTextWithLineHeight(
+            text: "\(currentPage)/\(data.count)",
+            fontStyle: .p5
+        )
     }
     
     // MARK: UI Components
@@ -81,53 +95,29 @@ final class MagazineViewController: UIViewController {
         label.layer.borderWidth = 1.5
         return label
     }()
-    
-    func configureUI() {
-        view.backgroundColor = ZiineColor.uiColor(.g900)
-        
-        view.addSubview(magazineCarousel)
-        magazineCarousel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(24)
-            $0.leading.trailing.equalToSuperview()
-        }
-        
-        view.addSubview(carouselPageCounter)
-        carouselPageCounter.snp.makeConstraints {
-            $0.top.equalTo(magazineCarousel.snp.bottom).inset(-12)
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(82)
-        }
-        
-        updatePageCounter()
-        
-    }
-    
-    private func updatePageCounter() {
-        carouselPageCounter.setTextWithLineHeight(
-            text: "\(currentPage - 2)/\(data.count)",
-            fontStyle: .p5
-        )
-    }
-    
-    // MARK: Infinity scroll
-    
-    private func configureInfinityScroll() {
-        let cellWidth = magazineCarousel.frame.width * CarouselConstants.visibleItemWidth + CarouselConstants.spacing
-        let targetOffset = CGPoint(
-            x: cellWidth * CGFloat(CarouselConstants.dummyItemCount),
-            y: 0
-        )
-        magazineCarousel.setContentOffset(targetOffset, animated: false)
-    }
 }
 
 // MARK: - MagazineViewPresentable
 
 extension MagazineViewController: MagazineViewPresentable {
     func reloadMagazineCarousel(magazineModels: [MagazineModel]) {
-        self.data = magazineModels
         magazineCarousel.reloadData()
-        configureInfinityScroll()
+    }
+}
+
+
+// MARK: - Scroll View Delegate (for Page Tracking)
+
+extension MagazineViewController: UIScrollViewDelegate {
+    func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
+        let layout = magazineCarousel.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        let estimatedIndex = (targetContentOffset.pointee.x / cellWidthIncludingSpacing).rounded()
+        currentPage = Int(estimatedIndex) + 1
     }
 }
 
@@ -142,7 +132,7 @@ extension MagazineViewController: UICollectionViewDelegate, UICollectionViewData
         _ collectionView: UICollectionView,
         numberOfItemsInSection section: Int
     ) -> Int {
-        return CarouselConstants.dummyItemCount * 2 + data.count
+        return data.count
     }
 
     func collectionView(
@@ -156,55 +146,7 @@ extension MagazineViewController: UICollectionViewDelegate, UICollectionViewData
             return UICollectionViewCell()
         }
         
-        let dataIndex = calculateDataIndex(for: indexPath.row)
-        cell.configure(with: data[dataIndex])
-        
+        cell.configure(with: data[indexPath.row])
         return cell
-    }
-    
-    private func calculateDataIndex(for row: Int) -> Int {
-        let totalItems = CarouselConstants.dummyItemCount * 2 + data.count
-        switch row {
-        case 0..<CarouselConstants.dummyItemCount:
-            return data.count - CarouselConstants.dummyItemCount + row
-        case totalItems - CarouselConstants.dummyItemCount..<totalItems:
-            return row - (totalItems - CarouselConstants.dummyItemCount)
-        default:
-            return row - CarouselConstants.dummyItemCount
-        }
-    }
-}
-
-// MARK: - Scroll View Delegate (for Page Tracking)
-extension MagazineViewController: UIScrollViewDelegate {
-    func scrollViewWillEndDragging(
-        _ scrollView: UIScrollView,
-        withVelocity velocity: CGPoint,
-        targetContentOffset: UnsafeMutablePointer<CGPoint>
-    ) {
-        let layout = magazineCarousel.collectionViewLayout as! UICollectionViewFlowLayout
-        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
-        let estimatedIndex = (targetContentOffset.pointee.x / cellWidthIncludingSpacing).rounded()
-        currentPage = Int(estimatedIndex) + 1
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let contentXOffset = scrollView.contentOffset.x
-        let numberOfItems = CarouselConstants.dummyItemCount * 2 + data.count
-        let cellWidth = magazineCarousel.frame.width * CarouselConstants.visibleItemWidth + CarouselConstants.spacing
-        
-        if Int(contentXOffset) > Int(cellWidth) * (numberOfItems - CarouselConstants.dummyItemCount) {
-            let targetOffset = CGPoint(
-                x: cellWidth * CGFloat(CarouselConstants.dummyItemCount),
-                y: 0
-            )
-            magazineCarousel.setContentOffset(targetOffset, animated: false)
-        } else if Int(contentXOffset) <= Int(cellWidth) {
-            let targetOffset = CGPoint(
-                x: cellWidth * CGFloat(numberOfItems - CarouselConstants.dummyItemCount - 1),
-                y: 0
-            )
-            magazineCarousel.setContentOffset(targetOffset, animated: false)
-        }
     }
 }
