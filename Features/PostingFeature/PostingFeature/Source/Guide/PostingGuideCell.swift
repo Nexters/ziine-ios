168bdab6ab9a3fdccf40dd6be5064778d1e406fe
GeonWoo <lgw9898@naver.com>
2025-02-15 19:44:23 +0900

    파일 구조 변경

---
+++ Features/PostingFeature/PostingFeature/Source/Guide/PostingGuideCell.swift
@@ -0,0 +1,47 @@
+//
+//  PostingGuideCell.swift
+//  PostingFeature
+//
+//  Created by Geon Woo lee on 2/15/25.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct PostingGuideCell: View {
+    private var guideMessage: PostingGuideMessage
+    private var index: Int
+    
+    init(index: Int, message: PostingGuideMessage) {
+        self.index = index
+        self.guideMessage = message
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("0\(index)")
+                .font(Font(ZiineFont.s1))
+                .foregroundStyle(.white)
+                .padding(.bottom, 4)
+            
+            Text(guideMessage.title)
+                .font(Font(ZiineFont.s1))
+                .padding(.bottom, 8)
+                .foregroundStyle(.white)
+            
+            Text(guideMessage.description)
+                .font(Font(ZiineFont.p3))
+                .foregroundStyle(.white)
+                .padding(.bottom, 14)
+            
+            ZiineImage.image(guideMessage.imageName)
+                .resizable()
+                .frame(height: 240)
+                .padding(.horizontal, 11.5)
+                .padding(.bottom, 24)
+                .foregroundStyle(.white)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 16)
+    }
+}
---
+++ Features/PostingFeature/PostingFeature/Source/Guide/PostingGuideView.swift
@@ -0,0 +1,66 @@
+//
+//  PostingGuideViewListener.swift
+//  PostingFeature
+//
+//  Created by Geon Woo lee on 2/15/25.
+//
+
+import SwiftUI
+import DesignSystem
+
+enum PostingGuideViewListener {
+    case postingButtonTapped
+}
+
+struct PostingGuideView: View {
+    public typealias Listener = ((PostingGuideViewListener) -> ())
+    
+    var listener: Listener?
+    
+    init(listener: Listener?) {
+        self.listener = listener
+    }
+    
+    private let guideMessage: [PostingGuideMessage] = PostingGuideMessage.make()
+    private let bottomButtonHeight: CGFloat = 52
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center, spacing: 12) {
+                Text("작가님의 작품을 등록하기 전,")
+                Text("꼭 확인해주세요!")
+            }
+            .font(Font(ZiineFont.h4))
+            .foregroundStyle(.white)
+            .padding(.vertical, 40)
+            
+            ForEach(Array(guideMessage.enumerated()), id: \ .offset) { index, message in
+                PostingGuideCell(index: index + 1, message: message)
+            }
+            .padding(.bottom, bottomButtonHeight)
+        }
+        .containerRelativeFrame(.horizontal)
+        .background(ZiineColor.color(.g900))
+        .overlay(alignment: .bottom) {
+            Button {
+                listener?(.postingButtonTapped)
+            } label: {
+                RoundedRectangle(cornerRadius: 16)
+                    .frame(height: bottomButtonHeight)
+                    .padding(.horizontal, 16)
+                    .foregroundStyle(ZiineColor.color(.p500))
+                    .overlay {
+                        Text("작품 등록하기")
+                            .font(Font(ZiineFont.custom(weight: .semiBold, size: 16)))
+                            .foregroundStyle(ZiineColor.color(.g900))
+                    }
+            }
+            .padding(.top, 12)
+            .background(ZiineColor.color(.g900))
+        }
+    }
+}
+
+#Preview {
+    PostingGuideView(listener: nil)
+}
