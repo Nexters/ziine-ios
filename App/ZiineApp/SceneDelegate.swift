21b7d57704918423ff264f2b548b2c5ac1c06dea
GeonWoo <lgw9898@naver.com>
2025-02-16 11:43:17 +0900

    다크모드 고정

--- App/ZiineApp/SceneDelegate.swift
+++ App/ZiineApp/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
+        window?.overrideUserInterfaceStyle = .dark
         
         let builder = AppRootBuilder()
         let routing = builder.build()
